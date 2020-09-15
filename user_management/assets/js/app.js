787f70eb9643f22ae02efc3fda7cab46c8e638f5
Thomas <thomas@craftacademy.se>
2020-09-15 15:17:27 +0200

    adds code for user management excercise

---
+++ user_management/assets/js/app.js
@@ -0,0 +1,42 @@
+const apiUrl = 'https://reqres.in/api/users'
+const proxyUrl = 'https://cors-anywhere.herokuapp.com'
+const usersDisplayNode = document.getElementById('users')
+
+const fetchUsers = async (page) => {
+  let data = await (await fetch(apiUrl + `?page=${page}&per_page=3`)).json()
+  return data
+}
+
+const fetchPage = page => {
+  fetchUsers(page).then(response => {
+    usersDisplayNode.innerHTML = ''
+    displayUsers(response)
+    dispalyNavigation(response)
+  })
+}
+
+const dispalyNavigation = response => {
+  let { page, total_pages } = response
+  let buttonsDisplay = document.createElement('div')
+  let prevLink = page > total_pages/total_pages ? `<button onclick="fetchPage(${page - 1})"><<</button>` : ''
+  let nextLink = page < total_pages ? `<button onclick="fetchPage(${page + 1})">>></button>` : ''
+  buttonsDisplay.innerHTML = prevLink + nextLink
+  usersDisplayNode.appendChild(buttonsDisplay)
+}
+
+const displayUsers = (response) => {
+  response.data.forEach(user => {
+    let userDiv = document.createElement('div')
+    userDiv.classList.add('grid-item')
+    let html = `<h2>${user.first_name}</h2>`
+    html += `<img src="${user.avatar}" />`
+    html += `<p><a href="mailto:${user.email}">${user.email}</a></p>`
+    userDiv.innerHTML = html
+    usersDisplayNode.appendChild(userDiv)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  usersDisplayNode.classList.add('grid-container')
+  fetchPage()
+})
